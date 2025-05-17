813c0d25daf3228009ad007667cbbc1ee5a38a56
Dedy Putra Aditia <73837147+dedyputra@users.noreply.github.com>
2025-05-18 00:01:36 +0700

    update catatan & database

---
+++ app/src/main/java/com/example/catatanharian/AddCatatanActivity.kt
@@ -0,0 +1,37 @@
+package com.example.catatanharian
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.catatanharian.databinding.ActivityAddCatatanBinding
+
+class AddCatatanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddCatatanBinding
+//    database
+    private lateinit var db: CatatanDatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddCatatanBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        db = CatatanDatabaseHelper(this)
+
+        binding.savaButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val catatan = Catatan(0, title, content)
+            db.insertCatatan(catatan)
+            finish()
+            Toast.makeText(this, "Simpan Catatan", Toast.LENGTH_SHORT).show()
+
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/catatanharian/Catatan.kt
@@ -0,0 +1,3 @@
+package com.example.catatanharian
+
+data class Catatan(val id: Int, val title: String, val content: String)
